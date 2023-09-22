6cf8ef63a1bac74291712b655063b1a920d90346
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-09-22 18:29:08 +0200

    theme styled

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './theme';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/react-project-ITSide-DrinkMaster">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
