7b32e8acc275c97bcf9697f58cd2324e49499790
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-09-22 22:42:06 +0300

    create styled for notfound

--- src/pages/NotFoundPage/NotFoundPage.styled.js
+++ src/pages/NotFoundPage/NotFoundPage.styled.js
@@ -1,1 +1,16 @@
+import styled from "@emotion/styled";
 
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 500px
+    `;
+
+export const Title = styled.h1`
+  color: #F3F3F3;
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+
+`;
