8e6b323d0feba141deb22b885909cb5d6eb4cb69
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-09-22 04:53:34 +0300

    startPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import SharedLayout from './components/SharedLayout/SharedLayout';
 import HomePage from './pages/HomePage/HomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import { AppWrapper } from './App.styled';
+import SignInPage from './pages/SignInPage/SignInPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import StartPage from './pages/StartPage/StartPage';
 
 const test = import.meta.env.VITE_API_TEST;
 
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/start" element={<StartPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
