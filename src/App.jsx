2ccb41b5d5c16ab1853929ded941f6a5aad5998f
Maksym Kravchenko <kmaksym@gmail.com>
2023-09-22 10:07:48 -0700

    select and radio button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import HomePage from './pages/HomePage/HomePage';
+// import HomePage from './pages/HomePage/HomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import { AppWrapper } from './App.styled';
+import AddDrinkPage from './pages/AddDrinkPage/AddDrinkPage';
+// import HomePage from './pages/HomePage/HomePage';
 
 const test = import.meta.env.VITE_API_TEST;
 
@@ -12,7 +14,7 @@ function App() {
     <AppWrapper>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<AddDrinkPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
---
+++ src/pages/AddDrinkPage/Components/DropDownList/DropDownMenu.jsx
@@ -0,0 +1,94 @@
+
+// import * as React from 'react';
+import { useState } from 'react';
+// import styled from '@emotion/styled';
+import Select from 'react-select';
+
+
+const DropDownMenu = ({category}) => {
+  const optionValue = [
+    { label: 'Cocktail glass', value: 'Cocktail glass' },
+    { label: ' glass', value: ' glass' },
+    { label: 'Cocktail ', value: 'Cocktail ' },
+    { label: 'Cocktail glass', value: 'Cocktail glass' },
+    { label: ' glass', value: ' glass' },
+    { label: 'Cocktail ', value: 'Cocktail ' },
+  ];
+  // const [value, setValue] = useState('');
+  // const colorStyles = {
+  //   dropdownIndicator: (styles) => {
+  //     console.log(styles);
+  //   },
+  // };
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const handleChange = ({value}) => {
+    category(value)
+  };
+  const customStyles = {
+    width:"300px",
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: 'transparent',
+      border: 0,
+      cursor: 'pointer',
+      borderColor: 'transparent',
+      boxShadow: 'none',
+      // '&:hover': {
+      //   // Define styles for when the control is hovered (optional)
+      //   borderColor: 'transparent',
+      // },
+    }),
+    option: (provided, { isFocused }) => ({
+      ...provided,
+      backgroundColor: '#161F37',
+      borderRadius:20,
+      color: isFocused ? '#F3F3F3' : '#F3F3F366',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: '#161F37',
+      borderRadius: 20,
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#F3F3F3',
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      cursor: 'pointer',
+        '&:hover': {
+        // Define styles for when the control is hovered (optional)
+        color: '#F3F3F3',
+      },
+      transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: 'none',
+    }),
+  };
+
+  const selectContainer = {
+    // width: '126px',
+    // flex:1
+  }
+  return (
+
+<div style={selectContainer}>
+      <Select
+        options={optionValue}
+        placeholder=""
+        onChange={handleChange}
+        styles={customStyles}
+        // colorStyles
+        // customStyles
+        onMenuOpen={() => setIsOpen(true)}
+        onMenuClose={() => setIsOpen(false)}
+      />
+</div>
+
+  );
+};
+
+export default DropDownMenu;
