4d1fd16e07fd4de89ee371c75a4237818621b4e9
Elena-Hrechyk <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-09-30 02:11:08 +0000

    Deploying to gh-pages from @ Elena-Hrechyk/react-project-ITSide-DrinkMaster@8acd2b2924849927b28fe38843f14ba5617db2c1 🚀

--- assets/MyDrinkPage-aefa9c2c.js
+++ assets/MyDrinkPage-aefa9c2c.js
@@ -1,4 +1,4 @@
-import{n as a,j as i}from"./index-65f6cb50.js";import{P as t,D as d}from"./Drinks_List-ef797e77.js";const n=a.main`
+import{n as a,j as i}from"./index-ad694668.js";import{P as t,D as d}from"./Drinks_List-5160518d.js";const n=a.main`
   font-family: 'Manrope',Arial,sans-serif;
   max-width: 480px;
   padding-left: 20px;
